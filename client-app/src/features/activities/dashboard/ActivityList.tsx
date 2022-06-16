41f6966bbd77a24c0c0de2e9ad4e60347feeb941
Hoai Phong <42806464+Hoaiphong0509@users.noreply.github.com>
2022-06-16 20:39:00 +0700

    End of section 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,14 @@
+import { observer } from 'mobx-react-lite'
 import React, { SyntheticEvent, useState } from 'react'
 import { Button, Item, Label, Segment } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
+import { useStore } from '../../../app/stores/store'
 
-interface Props {
-  activities: Activity[]
-  selectActivity: (id: string) => void
-  deleteActivity: (id: string) => void
-  submitting: boolean
-}
-
-const ActivityList = ({
-  activities,
-  selectActivity,
-  deleteActivity,
-  submitting,
-}: Props) => {
+const ActivityList = () => {
+  const { activityStore } = useStore()
+  const { activitiesByDate, deleteActivity , loading} = activityStore
   const [target, setTarget] = useState('')
+
   const handleActivityDelete = (
     e: SyntheticEvent<HTMLButtonElement>,
     id: string
@@ -27,7 +20,7 @@ const ActivityList = ({
   return (
     <Segment>
       <Item.Group divided>
-        {activities.map((activity) => (
+        {activitiesByDate.map((activity) => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as='a'>{activity.title}</Item.Header>
@@ -39,14 +32,14 @@ const ActivityList = ({
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(activity.id)}
+                  onClick={() => activityStore.selectActivity(activity.id)}
                   floated='right'
                   content='View'
                   color='blue'
                 />
                 <Button
                   name={activity.id}
-                  loading={submitting && target === activity.id}
+                  loading={loading && target === activity.id}
                   onClick={(e) => handleActivityDelete(e, activity.id)}
                   floated='right'
                   content='Delete'
@@ -62,4 +55,4 @@ const ActivityList = ({
   )
 }
 
-export default ActivityList
+export default observer(ActivityList)
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,21 +1,19 @@
+import { observer } from 'mobx-react-lite'
 import React, { useState } from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import { useStore } from '../../../app/stores/store'
 
-interface Props {
-  activity: Activity | undefined
-  closeForm: () => void
-  createOrEdit: (activity: Activity) => void
-  submitting: boolean
-}
+const ActivityForm = () => {
+  const { activityStore } = useStore()
+  const {
+    selectedActivity,
+    closeForm,
+    createActivity,
+    updateActivity,
+    loading,
+  } = activityStore
 
-const ActivityForm = ({
-  activity: selectedAcivity,
-  closeForm,
-  createOrEdit,
-  submitting,
-}: Props) => {
-  const initialState = selectedAcivity ?? {
+  const initialState = selectedActivity ?? {
     id: '',
     title: '',
     category: '',
@@ -28,7 +26,7 @@ const ActivityForm = ({
   const [activity, setActivity] = useState(initialState)
 
   const handleSubmit = () => {
-    createOrEdit(activity)
+    activity.id ? updateActivity(activity) : createActivity(activity)
   }
 
   const handleInputChange = (
@@ -79,7 +77,7 @@ const ActivityForm = ({
           onChange={handleInputChange}
         />
         <Button
-          loading={submitting}
+          loading={loading}
           floated='right'
           positive
           type='submit'
@@ -96,4 +94,4 @@ const ActivityForm = ({
   )
 }
 
-export default ActivityForm
+export default observer(ActivityForm)
